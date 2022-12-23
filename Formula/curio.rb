c583978953841749fc745cf62950663249d07a60
Cédric Fabianski <cedric@bearer.sh>
2022-12-23 16:15:04 +0100

    Update curio.rb

--- Formula/curio.rb
+++ Formula/curio.rb
@@ -5,22 +5,22 @@
 class Curio < Formula
   desc "A data-first security scanner that finds risks and vulnerabilities in your code so you can protect sensitive data (PHI, PD, PII)."
   homepage "https://curio.sh"
-  version "0.21.2"
+  version "0.21.1"
   license "Elastic License 2.0"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Bearer/curio/releases/download/v0.21.2/curio_0.21.2_darwin_arm64.tar.gz"
-      sha256 "fbcc52ec75f54cd2dde3f2b51dc19e14a230d540135b92825058a0d6fc449f06"
+      url "https://github.com/Bearer/curio/releases/download/v0.21.1/curio_0.21.1_darwin_arm64.tar.gz"
+      sha256 "61b5613f6a3e3d0b0bbff456f39e9bd19547af6fff3b25ce7bea6c2a0f28bb86"
 
       def install
         bin.install "curio"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Bearer/curio/releases/download/v0.21.2/curio_0.21.2_darwin_amd64.tar.gz"
-      sha256 "6dfbe1f18bd18b200a1a688ec3b1ecfdb6e85f526af812a01301de5793560693"
+      url "https://github.com/Bearer/curio/releases/download/v0.21.1/curio_0.21.1_darwin_amd64.tar.gz"
+      sha256 "ec5c9d8a35462e51196fc79dba2620e9a418f706fe2d881a4959ef1f68c0d6f0"
 
       def install
         bin.install "curio"
