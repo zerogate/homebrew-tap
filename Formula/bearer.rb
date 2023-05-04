da889d8e39903e84ec71545195a02894fe104f99
bearer <bearer@users.noreply.github.com>
2023-05-04 11:27:01 +0100

    chore: Brew formula update for bearer version v1.5.1

--- Formula/bearer.rb
+++ Formula/bearer.rb
@@ -5,21 +5,21 @@
 class Bearer < Formula
   desc "A data-first security scanner that finds risks and vulnerabilities in your code so you can protect sensitive data (PHI, PD, PII)."
   homepage "https://bearer.com"
-  version "1.4.0"
+  version "1.5.1"
   license "Elastic License 2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Bearer/bearer/releases/download/v1.4.0/bearer_1.4.0_darwin_arm64.tar.gz"
-      sha256 "79e8dc4d795c4c8ef40f9c23db3541e804ad7ec198eb0dced0078f3b36cbe312"
+      url "https://github.com/Bearer/bearer/releases/download/v1.5.1/bearer_1.5.1_darwin_arm64.tar.gz"
+      sha256 "e7c090df61ecadfdcdd01d88e3c6e9d38145a6c7df2c4aecdf8ba7764e7ba0f1"
 
       def install
         bin.install "bearer"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Bearer/bearer/releases/download/v1.4.0/bearer_1.4.0_darwin_amd64.tar.gz"
-      sha256 "67cdbafdc257ffafe23b576e5363274234dfc1c8fe938aba7bf4f1a79e38e280"
+      url "https://github.com/Bearer/bearer/releases/download/v1.5.1/bearer_1.5.1_darwin_amd64.tar.gz"
+      sha256 "b254b972791351674090f3688df7e42632679d0ef4dbb401f8a4b58523a47459"
 
       def install
         bin.install "bearer"
@@ -29,8 +29,8 @@ class Bearer < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Bearer/bearer/releases/download/v1.4.0/bearer_1.4.0_linux_amd64.tar.gz"
-      sha256 "04ce781e3ce78965d4d14dd71ae7c98cada5cfb8f2a004f4d033f78dd8a3142d"
+      url "https://github.com/Bearer/bearer/releases/download/v1.5.1/bearer_1.5.1_linux_amd64.tar.gz"
+      sha256 "6bb0073beebfebd2b46b1b9247543db0fcf7385f15abb70d87d7bf216250b222"
 
       def install
         bin.install "bearer"
